4e3d880f4adf482eb44c756c245b2ba65dcd562c
artur <arturkich6@gmail.com>
2025-02-27 17:49:54 -0300

    portugues

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        ingles();
+        portugues();
         
     }
-    public static void ingles() {
-        System.out.println("Hello, World!");
+   public static void portugues() {
+        System.out.println("Olá Mundo!");
     }
 }
